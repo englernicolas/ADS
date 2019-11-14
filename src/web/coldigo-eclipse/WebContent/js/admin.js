3e9d20ff2d1cbc8fe7f643d566290dabb7ff5ebc
englernicolas <nicolasengler@hotmail.com>
2019-11-14 20:56:21 -0300

    att coldigo, começando com db

--- src/web/coldigo-eclipse/WebContent/js/admin.js
+++ src/web/coldigo-eclipse/WebContent/js/admin.js
@@ -1,4 +1,17 @@
+COLDIGO = new Object();
+
 $(document).ready(function(){
 	$("header").load("/coldigo-eclipse/pages/admin/general/header.html");
 	$("footer").load("/coldigo-eclipse/pages/admin/general/footer.html");
+
+	COLDIGO.carregaPagina = function(pagename){
+		$("section").empty();
+		$("section").load(pagename+"/",function(response, status, info){ 
+			if (status == "error") {
+				var msg = "Houve um erro ao encontrar a página: " + info.status + " - " + info.statusText;
+				$("section").html(msg);
+			}
+		});
+		
+	}
 });
