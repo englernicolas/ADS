54e0e53b08a9d1d426fbca7ce8404c9be34459ea
Nicolas Engler <nicolasengler@hotmail.com>
2020-08-13 19:24:01 -0300

    live server

---
+++ src/projeto_individual/bibliotech/WebContent/assets/javascripts/components/EditProfile.js
@@ -0,0 +1,15 @@
+export default {
+    name: 'Home',
+    template: `<div>
+                    <div class="text-center my-5"><span class="text-h2 font-weight-bold">Minha Conta</span></div>
+                                        
+                    <v-divider></v-divider>
+                    
+                    <v-card class="mt-5">
+                        <div class="ml-16 text-h3">
+                            <v-avatar color="black"><v-icon dark>mdi-account-circle</v-icon></v-avatar>
+                            <span>Nome</span>
+                        </div>
+                    </v-card>
+               </div>`
+}
